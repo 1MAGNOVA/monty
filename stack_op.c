1b724ff8fd8832ce7c9696a52d1db1904731d430
1MAGNOVA <olalekanabsm@gmail.com>
2023-08-27 19:16:23 -0700

    monty

---
+++ stack_op.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * mul_nodes - Adds the top two elements of stack.
+ * @stack: Pointr to pointr points to stack. top node
+ * @line_number: Int reps line num of opcode.
+ */
+void mul_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "mul");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+
+/**
+ * mod_nodes - adds top two elements of stack.
+ * @stack: Pointr to a pointr points to stack. top node
+ * @line_number: Int reps the line num of opcode.
+ */
+void mod_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, line_number, "mod");
+
+
+	if ((*stack)->n == 0)
+		more_err(9, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n % (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
